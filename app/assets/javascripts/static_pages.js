cd388a7361d2fa93c539b56bba3e993afb7e0550
gittiggit <mailtommrr@gmail.com>
2016-02-16 11:26:38 +0600

    Commited after experiment with jQuery dom method: wrap, unwrap and wrapAll

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -6,6 +6,8 @@
 //});
 // 18937, 17717
 // to reduce the redundancy
+
+
 $(function(){
     $("li").css("color", "green").slideUp(2000).slideDown(2000).attr("title", "MY TITLE")
 
@@ -72,21 +74,33 @@ $(function(){
     $("#JSON-to-string").html(jsonString);
 
     // Converting JSON string to JSON object
-    var jsonString = [{"firstName":"Jamal","lastName":"Ahmed","gender":"Male","salary":50000},
-                        {"firstName":"Kamal","lastName":"Ahmed","gender":"Male","salary":50000},
-                        {"firstName":"Habib","lastName":"Ahmed","gender":"Male","salary":50000},
-                        {"firstName":"Montu","lastName":"Miah","gender":"Male","salary":50000}]
-    var jsonObject = JSON.parse(jsonString);
-
-    var result = ""
-    $.each(jsonObject, function(i, item){
-        result += "First Name = "+item.firstName+ "<br>";
-        result += "Last Name = "+item.lastName+ "<br>";
-        result += "Gender = "+item.gender+ "<br>";
-        result += "Salary= "+item.salary+ "<br>";
-    });
-    $("#JSON-string-to-object").html(result);
-
+    //var jsonString = [{"firstName":"Jamal","lastName":"Ahmed","gender":"Male","salary":50000},
+    //                    {"firstName":"Kamal","lastName":"Ahmed","gender":"Male","salary":50000},
+    //                    {"firstName":"Habib","lastName":"Ahmed","gender":"Male","salary":50000},
+    //                    {"firstName":"Montu","lastName":"Miah","gender":"Male","salary":50000}]
+    //var jsonObject = JSON.parse(jsonString);
+    //
+    //var result = ""
+    //$.each(jsonObject, function(i, item){
+    //    result += "First Name = "+item.firstName+ "<br>";
+    //    result += "Last Name = "+item.lastName+ "<br>";
+    //    result += "Gender = "+item.gender+ "<br>";
+    //    result += "Salary= "+item.salary+ "<br>";
+    //});
+    //$("#JSON-string-to-object").html(result);
 });
+
+
+$(function(){
+    $(".div1").wrap("<div class='containerDiv'></div>");
+    //$(".div1").unwrap();
+    $(".div2").wrapAll("<div class='containerDiv'></div>");
+});
+
+
+
+//$(document).ready(function(){
+//    $(".div1").wrap("<div class='containerDiv'></div>");
+//});
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   get "static_pages/methodhaining"
+  get "static_pages/wrapandunwrap"
   resources :products
   root "products#index"
 end
